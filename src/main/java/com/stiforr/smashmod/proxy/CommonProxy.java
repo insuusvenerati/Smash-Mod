530ccbbb98ab7564ed91aff8ae185dc60b4848df
Sean <norwood.sean@gmail.com>
2015-06-23 17:01:36 -0500

    weeeeee

---
+++ src/main/java/com/stiforr/smashmod/proxy/CommonProxy.java
@@ -0,0 +1,8 @@
+package com.stiforr.smashmod.proxy;
+
+public abstract class CommonProxy implements IProxy
+{
+	public int addArmor(String string) {
+		return 0;
+	}
+}
---
+++ src/main/java/com/stiforr/smashmod/proxy/IProxy.java
@@ -0,0 +1,5 @@
+package com.stiforr.smashmod.proxy;
+
+public interface IProxy {
+
+}
